5c2a79dc6cb98014a2d92f08f1976b5f06fa4b66
Neha Patel <neha.r.p@gmail.com>
2019-09-02 17:08:18 -0700

    get request coded in index.js

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const express = require('express');
 
 const server = express();
 
+//handle HTTP GET requests to the / URL
+server.get('/', (req, res) => {
+    res.send('hello world')
+});
+
 const port = 7000;
-server.listen(port, () => console.log('api running'));
+server.listen(port, () => console.log('\napi running\n'));
