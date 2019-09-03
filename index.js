98f3e659efc90cfb588775077594ad0b259d9268
Neha Patel <neha.r.p@gmail.com>
2019-09-02 17:15:42 -0700

    imported Hubs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //import
 const express = require('express');
 
+//import hubs-model file
+const Hubs = require('./data/hubs-model') //will use Hubs to get access to the DB
+//Hubs has find(), findById(), add(), remove(), update() methods
+
 const server = express();
 
 //handle HTTP GET requests to the / URL
@@ -8,5 +12,13 @@ server.get('/', (req, res) => {
     res.send('hello world')
 });
 
+//see a list of Hubs (similar to a channel on slack)
+
+//create a Hub
+
+//delete a Hub
+
+//update a Hub
+
 const port = 7000;
-server.listen(port, () => console.log('\napi running\n'));
+server.listen(port, () => console.log('\napi running\n')); //usually last line in file
